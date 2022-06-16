180f4aba8bdc84a49c92aa836769a81bdca2862f
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-16 20:49:39 +0200

    grey color ev.list added

--- main.js
+++ main.js
@@ -7,17 +7,32 @@ const btnBlack = document.createElement('button');
 const btnGrey = document.createElement('button');
 const btnSize = document.createElement('button');
 
-let createDiv = (row,col)=> {
-    for (let i = 1; i <= row*col ; i ++){
+let createDiv = (col,row)=> {
+    for (let i = 0; i < (row * col) ; i ++){
     const div = document.createElement('div');
-    container.appendChild(div);
+    container.appendChild(div).classList.add('box');
     div.style.border = `1px solid red`;
-    container.style.border = `1px solid black`;
-
-    container.style.gridTemplateColumns= `repeat (${col}, 1fr)`;
-    container.style.gridTemplateRows= `repeat (${row}, 1fr)`;
-    container.style.gridTemplateRows.border = `1px solid green`;
+   
+    container.style.gridTemplateColumns= `repeat(${col}, 1fr)`;
+    container.style.gridTemplateRows= `repeat(${row}, 1fr)`;
+    
     }
 }
 
-createDiv(16, 16);
+createDiv(6,6);
+
+
+function greyColor() {
+    const boxes = document.querySelectorAll('.box');
+    btnGrey.textContent = 'Grey';
+    btnGrey.addEventListener('click', e => {
+        boxes.forEach(box => box.addEventListener('mouseover', e => {
+            let randNum = Math.floor(Math.random() * 255);
+            box.style.background = `rgb(${randNum},${randNum},${randNum})`;
+            console.log(randNum);
+        }));
+    });
+    buttonsContainer.appendChild(btnGrey).classList.add('btn');
+}
+
+greyColor();
