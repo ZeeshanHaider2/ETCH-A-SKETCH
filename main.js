35dd2d47cfcfc6492153866bf4f3733ec91cb2ab
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-16 18:07:31 +0200

    css added

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const btnBlack = document.createElement('button');
 const btnGrey = document.createElement('button');
 const btnSize = document.createElement('button');
 
-let loop = (row,col)=> {
+let createDiv = (row,col)=> {
     for (let i = 1; i <= row*col ; i ++){
     const div = document.createElement('div');
     container.appendChild(div);
@@ -20,4 +20,4 @@ let loop = (row,col)=> {
     }
 }
 
-loop(15, 5);
+createDiv(16, 16);
