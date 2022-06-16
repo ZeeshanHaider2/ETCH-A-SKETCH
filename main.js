d9181e1f55a88a5535c45576fb01fd98911cfac4
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-16 23:12:39 +0200

    RGB function added

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const container = document.querySelector('.container');
 const buttonsContainer = document.querySelector('.buttons');
 const btnBlack = document.createElement('button');
 const btnGrey = document.createElement('button');
+const btnRGB = document.createElement('button');
 const btnSize = document.createElement('button');
 
 let createDiv = (col,row)=> {
@@ -22,17 +23,50 @@ let createDiv = (col,row)=> {
 createDiv(6,6);
 
 
-function greyColor() {
+let greyColor=()=> {
     const boxes = document.querySelectorAll('.box');
     btnGrey.textContent = 'Grey';
     btnGrey.addEventListener('click', e => {
         boxes.forEach(box => box.addEventListener('mouseover', e => {
             let randNum = Math.floor(Math.random() * 255);
             box.style.background = `rgb(${randNum},${randNum},${randNum})`;
-            console.log(randNum);
+            //console.log(randNum);
         }));
     });
     buttonsContainer.appendChild(btnGrey).classList.add('btn');
 }
 
 greyColor();
+
+let blackColor= ()=>{
+    const boxes = document.querySelectorAll('.box');
+    btnBlack.textContent = 'Black';
+    btnBlack.addEventListener('click', e => {
+        boxes.forEach(box => box.addEventListener('mouseover', e => {
+            
+            box.style.background = `black`;
+            
+        }));
+    });
+    buttonsContainer.appendChild( btnBlack).classList.add('btn');
+}
+
+blackColor();
+
+let rgbColor=()=> {
+    const boxes = document.querySelectorAll('.box');
+    btnRGB.textContent = 'RGB';
+    btnRGB.addEventListener('click', e => {
+        boxes.forEach(box => box.addEventListener('mouseover', e => {
+            let randNumR = Math.floor(Math.random() * 255);
+            let randNumG = Math.floor(Math.random() * 255);
+            let randNumB = Math.floor(Math.random() * 255);
+
+            box.style.background = `rgb(${randNumR},${randNumG},${randNumB})`;
+            //console.log(randNum);
+        }));
+    });
+    buttonsContainer.appendChild(btnRGB).classList.add('btn');
+}
+
+rgbColor();
